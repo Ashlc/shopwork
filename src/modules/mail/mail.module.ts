a25c54f104571fb163b71f466d394f4a6f3b1c8f
Isadora Paz <mibp@ic.ufal.br>
2025-10-14 10:42:28 -0300

    fix: used the wrong file lol

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,38 +1,9 @@
 import { Module } from '@nestjs/common';
-import { IMailService } from 'src/interfaces/services';
-import { MailController } from './mail.controller';
 import { MailService } from './mail.service';
+import { MailController } from './mail.controller';
 
 @Module({
   controllers: [MailController],
   providers: [MailService],
 })
-export class MailModule implements IMailService {
-  sendEmail(to: string, subject: string, body: string): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-
-  sendBulkEmail(
-    recipients: string[],
-    subject: string,
-    body: string,
-  ): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-
-  sendOrderConfirmation(userId: string, orderId: string): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-
-  sendClosureConfirmation(userId: string, orderId: string): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-
-  sendShippingInformation(userId: string, shipment: any): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-
-  sendPasswordResetEmail(userId: string, resetToken: string): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-}
+export class MailModule {}
--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { IMailService } from 'src/interfaces/services';
 
 @Injectable()
-export class MailService {}
+export class MailService implements IMailService {
+  sendEmail(to: string, subject: string, body: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  sendBulkEmail(
+    recipients: string[],
+    subject: string,
+    body: string,
+  ): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  sendOrderConfirmation(userId: string, orderId: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  sendClosureConfirmation(userId: string, orderId: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  sendShippingInformation(userId: string, shipment: any): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  sendPasswordResetEmail(userId: string, resetToken: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
