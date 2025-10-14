d3d52dba7045cb9ac58a4d086c8f8b3a864cc921
Isadora Paz <mibp@ic.ufal.br>
2025-10-14 13:44:28 -0300

    fix: dependency injection

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,26 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { FrameworkService } from './framework/framework.service';
-import { CartService } from './modules/cart/cart.service';
-import { CatalogService } from './modules/catalog/catalog.service';
-import { MailService } from './modules/mail/mail.service';
-import { OrderService } from './modules/order/order.service';
-import { PaymentService } from './modules/payment/payment.service';
-import { ProductService } from './modules/product/product.service';
-import { ReviewService } from './modules/review/review.service';
-import { ShippingService } from './modules/shipping/shipping.service';
-import { UserService } from './modules/user/user.service';
 
 @Injectable()
 export class AppService {
-  core = new FrameworkService(
-    new CartService(),
-    new CatalogService(),
-    new MailService(),
-    new OrderService(),
-    new PaymentService(),
-    new ProductService(),
-    new ReviewService(),
-    new ShippingService(),
-    new UserService(),
-  );
+  constructor(private readonly frameworkService: FrameworkService) {}
 }
